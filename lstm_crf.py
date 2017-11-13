0622026c418ce63ad9502bf906655ab9b64d37ba
JingningTang <tangjingning@gmail.com>
2017-11-12 23:44:12 -0600

    integrating to protein problem

--- lstm_crf.py
+++ lstm_crf.py
@@ -1,11 +1,15 @@
 from __future__ import print_function, division
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from tensorboardX import SummaryWriter
+
+from data_loading import Protein_Dataset
 
 
 # Helper functions to make the code more readable.
@@ -37,10 +41,9 @@ def log_sum_exp(vec):
 
 
 class BiLSTM_CRF(nn.Module):
-    def __init__(self, tag_to_ix, n_features, n_hidden, n_layers=1):
+    def __init__(self, n_features, n_hidden, n_layers=1):
         super(BiLSTM_CRF, self).__init__()
-        self.tag_to_ix = tag_to_ix
-        self.tagset_size = len(tag_to_ix)
+        self.tagset_size = 10 # 8 labels + START_TAG + STOP_TAG
 
         self.lstm = nn.LSTM(n_features, n_hidden // 2, n_layers, bidirectional=True)
         self.linear = nn.Linear(n_hidden, self.tagset_size)
@@ -48,40 +51,138 @@ class BiLSTM_CRF(nn.Module):
         # transitions[i,j] = the score of transitioning from j to i. Enforcing no 
         # transition from STOP_TAG and to START_TAG
         self.transitions = nn.Parameter(torch.randn(self.tagset_size, self.tagset_size))
-        self.transitions.data[tag_to_ix[START_TAG], :] = -10000
-        self.transitions.data[:, tag_to_ix[STOP_TAG]] = -10000
+        self.transitions.data[START_TAG, :] = -10000
+        self.transitions.data[:, STOP_TAG] = -10000
 
     def _get_lstm_features(self, prt_seqs):
         lstm_out, _ = self.lstm(prt_seqs)
-        print("LSTM output size:", lstm_out.size())
         lstm_feats = self.linear(lstm_out)
-        print("Linear output size:", lstm_feats.size())
         return lstm_feats
 
     def _score_sentence(self, feats, tags):
         # Gives the score of a ground truth tagged sequence
         score = autograd.Variable(torch.Tensor([0]))
-        tags = torch.cat([torch.LongTensor([self.tag_to_ix[START_TAG]]), tags])
+        tags = torch.cat([torch.LongTensor([START_TAG]), tags])
         for i, feat in enumerate(feats):
             score = score + \
                 self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
-        score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[-1]]
+        score = score + self.transitions[STOP_TAG, tags[-1]]
         return score
 
-    def forward(self, prt_seqs):
-        return self._get_lstm_features(prt_seqs)
+    def _forward_alg(self, feats):
+        # Do the forward algorithm to compute the partition function
+        init_alphas = torch.Tensor(1, self.tagset_size).fill_(-10000.)
+        # START_TAG has all of the score.
+        init_alphas[0][START_TAG] = 0.
+
+        # Wrap in a variable so that we will get automatic backprop
+        forward_var = autograd.Variable(init_alphas)
+
+        # Iterate through the sentence
+        for feat in feats:
+            alphas_t = []  # The forward variables at this timestep
+            for next_tag in range(self.tagset_size):
+                # broadcast the emission score: it is the same regardless of
+                # the previous tag
+                emit_score = feat[next_tag].view(
+                    1, -1).expand(1, self.tagset_size)
+                # the ith entry of trans_score is the score of transitioning to
+                # next_tag from i
+                trans_score = self.transitions[next_tag].view(1, -1)
+                # The ith entry of next_tag_var is the value for the
+                # edge (i -> next_tag) before we do log-sum-exp
+                next_tag_var = forward_var + trans_score + emit_score
+                # The forward variable for this tag is log-sum-exp of all the
+                # scores.
+                alphas_t.append(log_sum_exp(next_tag_var))
+            forward_var = torch.cat(alphas_t).view(1, -1)
+        terminal_var = forward_var + self.transitions[STOP_TAG]
+        alpha = log_sum_exp(terminal_var)
+        return alpha
+
+    def _viterbi_decode(self, feats):
+        backpointers = []
+
+        # Initialize the viterbi variables in log space
+        init_vvars = torch.Tensor(1, self.tagset_size).fill_(-10000.)
+        init_vvars[0][START_TAG] = 0
+
+        # forward_var at step i holds the viterbi variables for step i-1
+        forward_var = Variable(init_vvars)
+        for feat in feats:
+            bptrs_t = []  # holds the backpointers for this step
+            viterbivars_t = []  # holds the viterbi variables for this step
+
+            for next_tag in range(self.tagset_size):
+                # next_tag_var[i] holds the viterbi variable for tag i at the
+                # previous step, plus the score of transitioning
+                # from tag i to next_tag.
+                # We don't include the emission scores here because the max
+                # does not depend on them (we add them in below)
+                next_tag_var = forward_var + self.transitions[next_tag]
+                best_tag_id = argmax(next_tag_var)
+                bptrs_t.append(best_tag_id)
+                viterbivars_t.append(next_tag_var[0][best_tag_id])
+            # Now add in the emission scores, and assign forward_var to the set
+            # of viterbi variables we just computed
+            forward_var = (torch.cat(viterbivars_t) + feat).view(1, -1)
+            backpointers.append(bptrs_t)
+
+        # Transition to STOP_TAG
+        terminal_var = forward_var + self.transitions[STOP_TAG]
+        best_tag_id = argmax(terminal_var)
+        path_score = terminal_var[0][best_tag_id]
+
+        # Follow the back pointers to decode the best path.
+        best_path = [best_tag_id]
+        for bptrs_t in reversed(backpointers):
+            best_tag_id = bptrs_t[best_tag_id]
+            best_path.append(best_tag_id)
+        # Pop off the start tag (we dont want to return that to the caller)
+        start = best_path.pop()
+        assert start == START_TAG  # Sanity check
+        best_path.reverse()
+        return path_score, best_path
+
+    def neg_log_likelihood(self, sentence, tags):
+        feats = self._get_lstm_features(sentence)
+        forward_score = self._forward_alg(feats)
+        gold_score = self._score_sentence(feats, tags)
+        return forward_score, gold_score
+
+    def forward(self, sentence):  # dont confuse this with _forward_alg above.
+        # Get the emission scores from the BiLSTM
+        lstm_feats = self._get_lstm_features(sentence)
+
+        # Find the best path, given the features.
+        score, tag_seq = self._viterbi_decode(lstm_feats)
+        return score, tag_seq
+
 
 if __name__ == '__main__':
-    START_TAG = "<START>"
-    STOP_TAG = "<STOP>"
-    tag_to_ix = {key: value for (key, value) in \
-            zip(['L', 'B', 'E', 'G', 'I', 'H', 'S', 'T', START_TAG, STOP_TAG], range(10))}
-    model = BiLSTM_CRF(tag_to_ix, 66, 50)
-    prt_seqs = Variable(torch.rand(20, 1, 66))
-    print("Test input size:", prt_seqs.size())
-    lstm_out = model(prt_seqs)
-    for feat in lstm_out:
-        print(feat.size())
-        break
-    
+    max_seq_len = 698
+    batch_size = 64
+    epochs = 1
+    START_TAG = 8
+    STOP_TAG = 9
 
+    SetOf7604Proteins_path = os.path.expanduser('../data/SetOf7604Proteins/')
+    trainList_addr = 'trainList'
+    validList_addr = 'validList'
+    testList_addr = 'testList'
+
+    train_dataset = Protein_Dataset(SetOf7604Proteins_path, trainList_addr, max_seq_len)
+    valid_dataset = Protein_Dataset(SetOf7604Proteins_path, validList_addr, max_seq_len)
+    test_dataset = Protein_Dataset(SetOf7604Proteins_path, testList_addr, max_seq_len, padding=False)
+    len_train_dataset = len(train_dataset)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
+
+    model = BiLSTM_CRF(66, 50)
+
+    for epoch in range(epochs):
+        for i, (features, labels, lengths) in enumerate(test_loader):
+            features = Variable(features)
+            labels = Variable(labels)
