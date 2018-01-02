3f78036e75eaecda6a99671759b810abdfca8ebf
JingningTang <tangjingning@gmail.com>
2018-01-01 21:41:52 -0600

    latest working version. <72% valid_acc

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from __future__ import print_function, division
 import os
+import argparse
+import datetime
 import time
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
@@ -14,65 +15,95 @@ from model import LSTM_CRF, BiLSTM
 from utils import *
 
 max_seq_len = 698
-batch_size = 64
-epochs = 30
 START_TAG = 8
 STOP_TAG = 9
 
+parser = argparse.ArgumentParser(description='deep learning with CRF')
+parser.add_argument('run', metavar='DIR', help='specify the name of summary and model')
+parser.add_argument('-e', '--epochs', default=30, type=int, metavar='N', help='number of epochs to run (default: 30)')
+parser.add_argument('-b', '--batch_size', default=64, type=int, metavar='N', help='batch size of training data (default: 64)')
+parser.add_argument('--lr', default=1e-3, type=float, metavar='LR', help='initial learning rate (default: 1e-3)')
+parser.add_argument('--dropout', default=0.4, type=float, metavar='N', help='dropout factor. default: 0.4')
+parser.add_argument('-c', '--clean', action='store_true', default=False, help="If specified, clear the summary directory first")
+parser.add_argument('--hidden', default=100, type=int, metavar='N', help='number of hidden units in RNN (default: 100)')
+parser.add_argument('--layers', default=1, type=int, metavar='N', help='number of layers of RFF (default: 1)')
+args = parser.parse_args()
+
 use_cuda = torch.cuda.is_available()
 
+# Initialize dataloader
 SetOf7604Proteins_path = os.path.expanduser('../data/SetOf7604Proteins/')
-trainList_addr = 'trainList'
-validList_addr = 'validList'
-testList_addr = 'testList'
+train_dataset = Protein_Dataset(SetOf7604Proteins_path, 'trainList', max_seq_len)
+valid_dataset = Protein_Dataset(SetOf7604Proteins_path, 'validList', max_seq_len)
+test_dataset = Protein_Dataset(SetOf7604Proteins_path, 'testList', max_seq_len, padding=True)
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+valid_loader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
 
-train_dataset = Protein_Dataset(SetOf7604Proteins_path, trainList_addr, max_seq_len)
-valid_dataset = Protein_Dataset(SetOf7604Proteins_path, validList_addr, max_seq_len)
-test_dataset = Protein_Dataset(SetOf7604Proteins_path, testList_addr, max_seq_len, padding=True)
-len_train_dataset = len(train_dataset)
+# Save model information
+writer_path = os.path.join("logger", args.run)
+if os.path.exists(writer_path) and args.clean:
+    shutil.rmtree(writer_path, ignore_errors=True)
+save_model_dir = os.path.join(os.getcwd(), "saved_model")
+if not os.path.exists(save_model_dir):
+    os.makedirs(save_model_dir)
+model_name = ['rnn', datetime.datetime.now().strftime("%b%d_%H:%M"), args.run,
+              'epochs', str(args.epochs), 'lr', str(args.lr),
+              'hidden', str(args.hidden), 'layers', str(args.layers)]
+model_name = '_'.join(model_name)
+model_path = os.path.join(save_model_dir, model_name)
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-
-# model = BiLSTM(100, 8)
-ix_counter = 0
-writer_path = os.path.join("logger", 'test' + str(ix_counter))
-while os.path.exists(writer_path):
-    ix_counter += 1
-    writer_path = os.path.join("logger", 'test' + str(ix_counter))
 writer = SummaryWriter(log_dir=writer_path)
 
-model = LSTM_CRF(100)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+# model = BiLSTM(args.hidden, 8, args.dropout, args.layers)
+model = LSTM_CRF(args.hidden, args.layers, args.dropout)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 criterion = nn.CrossEntropyLoss()
 
 if use_cuda:
     model = model.cuda()
     criterion =criterion.cuda()
 
-
-for epoch in range(epochs):
+best_accuracy = 0
+len_train_dataset = len(train_dataset)
+start = time.time()
+for epoch in range(args.epochs):
     model.train()
     step_counter = 0
     for i, (features, labels, lengths) in enumerate(train_loader):
         features = cuda_var_wrapper(features.transpose(0, 1))
         labels = cuda_var_wrapper(labels.transpose(0, 1))
         features, labels, lengths = sort_by_length(features, labels, lengths)
-        output = model.decode(features, lengths)
+        loss = model(features, labels, lengths)
+        # output = model(features, lengths)
+        # loss = criterion(output.view(-1, 8), labels.view(-1))
         optimizer.zero_grad()
-        loss = criterion(output.view(-1, 8), labels.view(-1))
         step_counter += features.size()[1]
         writer.add_scalar('data/loss', loss.data[0], step_counter + epoch * len_train_dataset)
         loss.backward()
         optimizer.step()
         if i % 20 == 0:
             print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f' 
-               %(epoch+1, epochs, i, len_train_dataset//batch_size, loss.data[0]))
-        # if i == 5: break
+               %(epoch+1, args.epochs, i, len_train_dataset//args.batch_size, loss.data[0]))
 
     accuracy_train = evaluate(model, train_loader)
     accuracy_valid = evaluate(model, valid_loader)
+    if accuracy_valid > best_accuracy:
+        best_accuracy = accuracy_valid
+        torch.save(model.state_dict(), model_path)
     writer.add_scalars('data/accuracy_group', {'accuracy_train': accuracy_train,
                                                'accuracy_valid': accuracy_valid}, (epoch + 1) * len_train_dataset)
     print("Training accuracy {:.4f}; Validation accuracy {:.4f}".format(accuracy_train, accuracy_valid))
+
+print("Time spent on training: {:.2f}s".format(time.time() - start))
+torch.save(model.state_dict(), model_path + '_2')
+writer.close()
+
+model.load_state_dict(torch.load(model_path))
+accuracy_test = evaluate(model, test_loader)
+print("Model for best validation accuracy:")
+print("Test accuracy {:.3f}".format(accuracy_test))
+model.load_state_dict(torch.load(model_path + '_2'))
+accuracy_test = evaluate(model, test_loader)
+print("Model for complete training:")
+print("Test accuracy {:.3f}".format(accuracy_test))
--- model.py
+++ model.py
@@ -6,33 +6,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.autograd import Variable
+from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from crf import CRF
+from data_loading import Protein_Dataset
+from utils import *
 
 
-class BiLSTM(nn.Module):
-    def __init__(self, n_hidden, linear_out, n_layers=1):
-        super(BiLSTM, self).__init__()
-        self.lstm = nn.LSTM(66, n_hidden // 2, n_layers, bidirectional=True)
-        self.linear = nn.Linear(n_hidden, linear_out)
-
-    def forward(self, features, lengths):
-        packed_input = pack_padded_sequence(features, lengths)
-        output, _ = self.lstm(packed_input)
-        output, _ = pad_packed_sequence(output)
-        output = self.linear(output)
-        return output
+use_cuda = torch.cuda.is_available()
+START_TAG = 8
+STOP_TAG = 9
 
 
 class LSTM_CRF(nn.Module):
-    def __init__(self, n_hidden, n_layers=1):
+    def __init__(self, n_hidden, n_layers=1, dropout_rate=0.5):
         super(LSTM_CRF, self).__init__()
-        self.lstm = BiLSTM(n_hidden, 10, n_layers=n_layers)
+        self.lstm = BiLSTM(n_hidden, 10, dropout_rate=dropout_rate, n_layers=n_layers)
         self.crf1 = CRF()
 
     def forward(self, features, labels, lengths):
         lstm_feats = self.lstm(features, lengths)
-        labels = labels.data
         score = self.crf1.neg_log_likelihood(lstm_feats, labels)
         return score
 
@@ -40,3 +32,155 @@ class LSTM_CRF(nn.Module):
         lstm_feats = self.lstm(features, lengths)
         _, output = self.crf1(lstm_feats)
         return output
+
+
+class BiLSTM(nn.Module):
+    def __init__(self, n_hidden, linear_out, dropout_rate, n_layers=1):
+        super(BiLSTM, self).__init__()
+        self.lstm = nn.LSTM(66, n_hidden // 2, n_layers, bidirectional=True)
+        self.linear = nn.Linear(n_hidden, linear_out)
+        self.dropout = nn.Dropout(p=dropout_rate)
+
+    def forward(self, features, lengths):
+        packed_input = pack_padded_sequence(features, lengths)
+        output, _ = self.lstm(packed_input)
+        output, _ = pad_packed_sequence(output)
+        output = self.linear(output)
+        output = self.dropout(output)
+        return output
+
+
+class CRF(nn.Module):
+    def __init__(self):
+        super(CRF, self).__init__()
+        self.tagset_size = 10 # 8 labels + START_TAG + STOP_TAG
+        # transitions[i,j] = the score of transitioning from j to i. Enforcing no 
+        # transition from STOP_TAG and to START_TAG
+        self.transitions = nn.Parameter(torch.randn(self.tagset_size, self.tagset_size))
+        self.transitions.data[START_TAG, :] = -10000
+        self.transitions.data[:, STOP_TAG] = -10000
+
+
+    def _score_sequence(self, feats, labels):
+        """Gives the scores of a batch of ground truth tagged sequences
+        feats: [sequence_length, batch_size, tagset_size]
+        labels: [sequence_length, batch_size]
+        """
+        batch_size = feats.size()[1]
+        score = cuda_var_wrapper(torch.zeros(1, batch_size))
+        start_labels = cuda_var_wrapper(torch.LongTensor(1, batch_size).fill_(START_TAG))
+        stop_labels = cuda_var_wrapper(torch.LongTensor(1, batch_size).fill_(STOP_TAG))
+        emit = feats.gather(2, labels.unsqueeze(2)).squeeze()
+        labels = torch.cat((start_labels, labels), 0)
+        for i, feat in enumerate(feats):
+            trans = self.transitions[labels[i + 1], labels[i]].unsqueeze(0)
+            # print('trans.size():', trans.size())
+            score += trans
+        score = score + self.transitions[stop_labels, labels[-1]] + emit.sum(0,keepdim=True)
+        return score.squeeze()
+
+
+    def _forward_alg(self, feats):
+        """Do the forward algorithm to compute the partition function
+        feats: [sequence_length, batch_size, tagset_size]
+        """
+        init_alphas = torch.Tensor(feats.size()[1:]).fill_(-10000.)
+        init_alphas[:, START_TAG] = 0.
+        forward_var = Variable(init_alphas.unsqueeze(1))
+        if use_cuda:
+            forward_var = forward_var.cuda()
+        # Iterate through the sentence
+        for feat in feats:
+            # feat.size() = batch_size x tagset_size
+            emit_score = feat.unsqueeze(2)
+            # [batch, tagset, tagset] = [batch, 1, tagset] + [tagset, tagset] + [batch, tagset, 1]
+            tag_var = forward_var + self.transitions + emit_score
+            forward_var = log_sum_exp(tag_var).unsqueeze(1)
+        terminal_var = (forward_var + self.transitions[STOP_TAG]).squeeze()
+        alpha = log_sum_exp(terminal_var)
+        return alpha
+
+
+    def _viterbi_decode(self, feats):
+        """Given the nn extracted features [Seq_len x Batch_size x tagset_size], return the Viterbi
+           Decoded score and most probable sequence prediction. Input feats is assumed to have
+           dimension of 3.
+        """
+        batch_size = feats.size()[1]
+        backpointers = []
+        init_vvars = torch.Tensor(feats.size()[1:]).fill_(-10000.)
+        init_vvars[:, START_TAG] = 0
+        forward_var = Variable(init_vvars.unsqueeze(1)) # [batch x 1 x tagset]
+        if use_cuda:
+            forward_var = forward_var.cuda()
+
+        for feat in feats:
+            # next_tag_var: [batch x tagset x tagset]
+            next_tag_var = forward_var + self.transitions
+            # viterbi_vars: [batch x tagset]
+            viterbi_vars, best_tag_id = next_tag_var.max(2)
+            forward_var = (viterbi_vars + feat).unsqueeze(1)
+            # list of [batch x tagset], equivalent to [seq_len x batch x tagset]
+            backpointers.append(best_tag_id)
+
+        # terminal_var: [batch x tagset]
+        terminal_var = (forward_var + self.transitions[STOP_TAG]).squeeze()
+        path_score, best_tag_id = terminal_var.max(1)
+
+        best_path = [best_tag_id.unsqueeze(0)] # [batch]
+        for backpointer in reversed(backpointers):
+            # backpointer: [batch x tagset]
+            best_tag_id = backpointer[range(batch_size), best_tag_id.data]
+            best_path.append(best_tag_id.unsqueeze(0))
+        start = best_path.pop()
+        # assert start[:, 0] == START_TAG
+        best_path = torch.cat(best_path[::-1], 0)
+        return path_score, best_path
+
+
+    def neg_log_likelihood(self, feats, labels):
+        forward_score = self._forward_alg(feats)
+        gold_score = self._score_sequence(feats, labels)
+        return torch.mean(forward_score - gold_score)
+
+
+    def forward(self, feats):  
+        score, tag_seq = self._viterbi_decode(feats)
+        return score, tag_seq
+
+
+# For testing only
+if __name__ == '__main__':
+    max_seq_len = 200
+    batch_size = 2
+
+    SetOf7604Proteins_path = os.path.expanduser('../data/SetOf7604Proteins/')
+    testList_addr = 'testList'
+    test_dataset = Protein_Dataset(SetOf7604Proteins_path, testList_addr, max_seq_len, padding=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    lstm = BiLSTM(100, 10)
+    model = CRF()
+    for i, (features, labels, lengths) in enumerate(test_loader):
+        features = cuda_var_wrapper(features.transpose(0, 1))
+        labels = cuda_var_wrapper(labels.transpose(0, 1))
+        features, labels, lengths = sort_by_length(features, labels, lengths)
+        feat = lstm(features, lengths)
+        print(feat.size())
+        print(labels.size())
+        # test = model.neg_log_likelihood(feat, labels.data)
+        # score1, path1 = model._viterbi_decode(feat)
+        score1 = model._score_sequence(feat, labels)
+        # path1 = path1.t().data.tolist()
+        score, path = [], []
+        for j in range(batch_size):
+            # score2, path2 = model._viterbi_decode_origin(feat[:, j, :])
+            score2 = model._score_sentence_original(feat[:, j, :], labels.data[:, j])
+            score.append(score2)
+            # path.append(path2)
+        score = torch.cat(score)
+        print(score1, score)
+        # print(path1)
+        # print(path)
+        # print(path1 == path)
+        if i == 0: break
